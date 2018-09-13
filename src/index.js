fc8de01f7c30fa20f0cb010158bb8743f8b5e812
“Simaxa7” <simaxa7@mail.ru>
2018-09-14 00:27:56 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  
+	var zeros=0;
+
+	for (let i = 1; i < 13; i++) {
+		let numberDivide = number / Math.pow(5,i);
+		
+		if (numberDivide >= 1){
+			zeros += Math.floor(numberDivide);
+		}else{
+			break;
+		}
+	}
+	return zeros
 }
